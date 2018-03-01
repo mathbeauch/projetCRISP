aad9138391a2d29daa074eea2b23159ae656e8ba
steefanr <steefan.r@hotmail.com>
2018-03-01 12:36:35 -0500

    Ajout de la premiere fonction de test Tacho

--- Tests/ControleurLED/mainTestScript.m
+++ Tests/ControleurLED/mainTestScript.m
@@ -1,4 +1,9 @@
 %%Initialiser les valeurs du controleur
-
+port = 'COM3'; % Port de la connexion
 ad = 1; % Adresse du controleur
-port = 'COM3'; 
+fctID = 14; % Fonction pour Set Output
+fctTYPE = 0;
+motor = 2; % OUTPUT du controleur
+value =1;
+
+SendCommand(port, ad, fctID, fctTYPE, motor,value);
